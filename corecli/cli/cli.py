8ef2865624d1827439cdc9bb2f7c321e38592a95
timfeirg <kkcocogogo@gmail.com>
2017-02-23 13:21:48 +0800

    add zone support

--- corecli/cli/cli.py
+++ corecli/cli/cli.py
@@ -10,11 +10,12 @@ from corecli.cli.utils import read_json_file, write_json_file, get_username
 
 
 @click.group()
+@click.option('--zone', help='citadel zone, default to c2')
 @click.option('--config-path', default=expanduser('~/.corecli.json'), help='config file, json', envvar='CITADEL_CONFIG_PATH')
 @click.option('--remotename', default='origin', help='git remote name, default to origin', envvar='CORECLI_REPO_NAME')
 @click.option('--debug', default=False, help='enable debug output', envvar='CORECLI_DEBUG', is_flag=True)
 @click.pass_context
-def core_commands(ctx, config_path, remotename, debug):
+def core_commands(ctx, zone, config_path, remotename, debug):
     config = read_json_file(config_path)
     if not config:
         config = {}
@@ -26,7 +27,11 @@ def core_commands(ctx, config_path, remotename, debug):
         write_json_file(config, config_path)
         click.echo('config saved to {}'.format(config_path))
 
-    ctx.obj['coreapi'] = CoreAPI(config['citadel_url'].strip('/'), username=config['username'], auth_token=config['auth_token'])
+    coreapi = CoreAPI(config['citadel_url'].strip('/'), username=config['username'], auth_token=config['auth_token'])
+    if zone:
+        coreapi.zone = zone
+
+    ctx.obj['coreapi'] = coreapi
     ctx.obj['remotename'] = remotename
     ctx.obj['mimironurl'] = config['mimiron_url']
     ctx.obj['debug'] = debug
